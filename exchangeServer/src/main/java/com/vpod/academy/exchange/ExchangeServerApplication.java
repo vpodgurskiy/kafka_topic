8ab4eb305e240123383c20fe2499fc65d8e088f8
Vadim <PodgurskiiVI@suek.ru>
2023-01-26 21:53:07 +0300

    create project structure

---
+++ exchangeServer/src/main/java/com/vpod/academy/exchange/ExchangeServerApplication.java
@@ -0,0 +1,12 @@
+package com.vpod.academy.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExchangeServerApplication.class, args);
+  }
+}
